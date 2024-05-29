05389e449d58952a77037c814e3f482a5dfd6b11
Hasnaat1 <hasnaatdon1@gmail.com>
2024-05-29 06:01:07 +0500

    variables complete

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // Ch1 Variables & datatypes
+// VARIABLES
 console.log("Hello world");
 name = "HEHEHEHHEE"
-console.log(name);
+var print = "hello world"// In var value can be redeclared or update we dont usually use this
+let fullName = "Main nhi btaon ga"
+let age = 24            // In let value cannot redeclared but update
+let mr = "bean"
+console.log(fullName);
+const pi = 3.14;          //In const value cannot redeclared or update
+console.log(pi);
+// Datatypes
+// primitive(7) and non-primitive
+// Number,String,Boolean,undefined,null,BigInt,Symbol
