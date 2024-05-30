6ae2c03f471daa81c6e38f00304898383a423cac
Hasnaat1 <hasnaatdon1@gmail.com>
2024-05-30 07:38:07 +0500

    datatypes complete

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // Ch1 Variables & datatypes
 // VARIABLES
-console.log("Hello world");
-name = "HEHEHEHHEE"
-var print = "hello world"// In var value can be redeclared or update we dont usually use this
-let fullName = "Main nhi btaon ga"
-let age = 24            // In let value cannot redeclared but update
-let mr = "bean"
-console.log(fullName);
-const pi = 3.14;          //In const value cannot redeclared or update
-console.log(pi);
+// console.log("Hello world");
+// name = "HEHEHEHHEE"
+// var print = "hello world"// In var value can be redeclared or update we dont usually use this
+// let fullName = "Main nhi btaon ga"
+// let age = 24            // In let value cannot redeclared but update
+// let mr = "bean"
+// console.log(fullName);
+// const pi = 3.14;          //In const value cannot redeclared or update
+// console.log(pi);
 // Datatypes
-// primitive(7) and non-primitive
+// primitive(7) and non-primititsve
 // Number,String,Boolean,undefined,null,BigInt,Symbol
+// non-primitive ->objects 1-Arrays 2-functions
+// -> objects 
+const student = {
+    name : "Hasnaat",
+    age : 20,
+    rollNo : 601,
+    isPass : true,
+};
