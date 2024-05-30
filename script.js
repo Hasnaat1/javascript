456d5f3edc3043e9616c7cea1d07eb434a3a0fd0
Hasnaat1 <hasnaatdon1@gmail.com>
2024-05-30 09:28:36 +0500

    conditional statements doing

--- script.js
+++ script.js
@@ -14,9 +14,64 @@
 // Number,String,Boolean,undefined,null,BigInt,Symbol
 // non-primitive ->objects 1-Arrays 2-functions
 // -> objects 
-const student = {
-    name : "Hasnaat",
-    age : 20,
-    rollNo : 601,
-    isPass : true,
-};
+// const student = {
+//     name : "Hasnaat",
+//     age : 20,
+//     rollNo : 601,
+//     isPass : true,
+// };
+// // Q1
+// const product = {
+//     title : "Ball Pen",
+//     rating : 4,
+//     offer : 5,
+//     price : 250,
+// };
+// console.log(product)
+// operator in js + - * /
+// let a = 54;
+// let b = 4;
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// // console.log(a**b) 2^b
+
+// Unary operator
+// a++;
+// a--;
+// --a;
+// ++a;
+// console.log("a=",a+"b=",b);
+
+// Assignment Operators
+// a =+ 1;
+//comparison operatoryoun
+// ! = > <
+
+// logical operators
+// AND OR NOT
+
+// Conditional Statements
+let age = 18;
+if (age => 18){
+    console.log("You are young ");
+}
+let mode = "dark";
+let color;
+if (mode==="dark"){
+    color = "black";
+}
+else{
+    color="White";
+}
+console.log(color);
+
+// odd even 
+let num = 24;
+if (num%2 === 0){
+    console.log(num,"is odd")
+}else{
+    console.log(num,"is even")
+}
